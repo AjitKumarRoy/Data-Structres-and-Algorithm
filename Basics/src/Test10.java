974ed2e8e1258e24ff56a73758732dd4f97fd38e
AjitKumarRoy <ak47riderji@gmail.com>
2023-10-07 10:31:36 +0530

    leetcode practice

--- Basics/src/Test10.java
+++ Basics/src/Test10.java
@@ -36,10 +36,65 @@
 import java.util.*;
 public class Test10 {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
-        int target = 0;
+        int[] arr = {3,5,1};
+        int target = 3;
 
-        System.out.println(pivot(arr));
+        //calling searchTarget function
+        System.out.println(searchTarget(arr, target));
+    }
+
+    //function to search for target in an rotated sorted array
+    public static int searchTarget(int[] arr, int target) {
+        int pivet = pivot(arr);
+
+        //if pivet == -1, that means the array is not rotated
+        //just apply the binary search from start to end
+        if (pivet == -1) {
+            return binarySearch(arr, target, 0, arr.length-1);
+        }
+
+        //if pivot is found, then it means you have two ascending sorted arrays
+        //now search the target in both the ascending sorted arrays
+
+        //check if target element is equal to the element at pivot
+        if (target == arr[pivet]) {
+            //if target found, then return pivet
+            return pivet;
+        }
+        //check if target is grater than the first element
+        else if (target >= arr[0]) {
+            //search left
+            return binarySearch(arr, target, 0, pivet-1);
+        }
+        //check if target is less than the first element
+        else {
+            //search right
+            return binarySearch(arr, target, pivet+1, arr.length-1);
+        }
+    }
+
+    //binary search function
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+        //iterate till start is less than or equal to end
+        while (start <= end) {
+            //find the middle element
+            int mid = start + (end - start) / 2;
+
+            //check if target element is equal to mid element
+            if (target == arr[mid]) {
+                return mid;
+            }
+            //check if target element is less than the mid element
+            else if (target < arr[mid]) {
+                end = mid - 1;
+            }
+            //check if target element is greater than the mid element
+            else {
+                start = mid + 1;
+            }
+        }
+        //if target not found, then return -1
+        return -1;
     }
 
     //function to find the pivot
@@ -47,31 +102,24 @@ public class Test10 {
         int start = 0;
         int end = arr.length - 1;
 
-        while (start < end) {
+        //iterate till start is less than or equal to end
+        while (start <= end) {
             //find the middle element
             int mid = start + (end - start) / 2;
 
-            if (arr[mid] > arr[mid+1]) {
+            if (mid < end && arr[mid] > arr[mid+1]) {
                 return mid;
             }
-
-            if (arr[mid] < arr[mid-1]) {
+            if (mid > start && arr[mid] < arr[mid-1]) {
                 return mid-1;
             }
 
-            if (arr[mid + 1] > arr[mid]) {
-                start = mid + 1;
-            }
-            else if (arr[mid+1] <= arr[mid]) {
-                end = mid;
-            }
-            else if (arr[mid+1] > arr[mid] && arr[mid] < arr[start]) {
-                end = mid;
-            }
-            else {
+            if(arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
                 start = mid + 1;
             }
         }
-        return start;
+        return -1;
     }
 }
